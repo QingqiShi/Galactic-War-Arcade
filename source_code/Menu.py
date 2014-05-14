d345ccc7b56d10311c4954852133b69c35be621a
QingqiShi <qingqishi@live.com>
2014-05-14 08:18:55 +0100

    update diagram doc

--- source_code/Menu.py
+++ source_code/Menu.py
@@ -8,7 +8,8 @@ class Menu(object):
         #        2 = Pause
         #        3 = Game Over
 
-        if menuID == 3:
+        if menuID == 2:
+        elif menuID == 3:
             screen.blit(pygame.font.SysFont("monospace", 72, True).render("Game Over", 1, (91, 109, 131)), (320, 300))
             screen.blit(pygame.font.SysFont("monospace", 16, True).render("Press R to restart", 1, (91, 109, 131)), (420, 370))
             screen.blit(pygame.font.SysFont("monospace", 16, True).render("Score", 1, (91, 109, 131)), (380, 420))
